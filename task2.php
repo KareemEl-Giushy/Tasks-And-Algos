9cb95b1a22a5c48e53358a8f9d4eac14de20972a
Kareem El-Giushy <kareem.64123@gmail.com>
2020-12-15 01:32:34 +0200

    Sorting Algorithem With php

---
+++ task2.php
@@ -0,0 +1,23 @@
+<?php 
+    
+    function arraysort($arr = []) {
+        for ($i = 0; $i < count($arr);$i++){
+            // getting the first value
+            for($k = $i+1; $k < count($arr);$k++){
+                // comparing with the rest of valuses
+                if($arr[$k][0] < $arr[$i][0]) {
+                    // if yes swop it to the head of the array
+                    $temp = $arr[$i];
+                    $arr[$i] = $arr[$k];
+                    $arr[$k] = $temp;
+                }
+            }
+        }
+
+        // print the array
+        print_r($arr);
+    }
+
+
+    // Call Function
+    arraysort(['eman', 'ali', 'osama', 'yasser']);
